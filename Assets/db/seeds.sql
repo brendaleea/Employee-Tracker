182512bb9055e3b91849fd7c73e8fd9c8c53c60b
ba23 <blsarabia7@gmail.com>
2023-12-18 18:33:56 -0700

    created directory and contents

---
+++ Assets/db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments (department_name)
+VALUES 
+('Finance'),
+('Marketing'),
+('Human Resources'),
+('Customer Service'),
+('Information Technology'),
+
+
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+('Finance Manager', 120000.00, 1),
+('Marketing Manager', 98000.00, 2)
+('HR Director', 90000.00, 3),
+('Customer Service Manager', 65000.00, 4),
+('IT Manager', 115000.00, 5),
+
+
+
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('John', 'Smith', 1, 1),
+('Brendalee', 'Alcala', 2, 2),
+('Tom', 'Hardy', 3, 3),
+('August', 'Flowers', 4, 4),
+('Alayna', 'Chase', 5, 5),
